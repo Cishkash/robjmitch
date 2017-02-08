ff7ceffd0fab1eaffccc0f2760a191925df408dd
Robby Mitchell <robjmitch@gmail.com>
2017-02-07 22:13:17 -0800

    More groundwork for allowing a user to log into admin

--- src/components/AdminPortal.js
+++ src/components/AdminPortal.js
@@ -1,9 +1,50 @@
 import React, { Component } from 'react';
 
-class Admin extends Component {
+/**
+ * The Admin component. Allows a user to log in to firebase to make or update
+ * blog posts.
+ *
+ * @class Component.AdminPortal
+ * @extends React.Component
+ */
+class AdminPortal extends Component {
+  /**
+   * The constructor for the admin class. Initializes state of the input fields
+   *
+   * @event constructor
+   * @return {undefined}
+   */
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+  /**
+   * Sends the authentication credentials to firebase to authenticate.
+   *
+   * @method authenticate
+   * @return {undefined}
+   */
   authenticate() {
-    console.log('You attempted to authenticate');
+    console.log("Logging into Firebase admin");
     return;
+  }
+  /**
+   * Updates the value of the email or password state.
+   *
+   * @method handleChange
+   * @param  {object} evt The event object when chaning the DOM
+   * @return {undefined}
+   */
+  handleChange = evt => {
+    const target = evt.target;
+    const name = target.name;
+
+    this.setState({
+      [name]: target.value
+    });
   }
   /**
    * Render method. Renders the layout of the Admin component.
@@ -22,10 +63,14 @@ class Admin extends Component {
               <div className="card-block text-right">
                 <input type="email" name="email"
                        className="form-control"
-                       placeholder="Email address"/><br/>
+                       placeholder="Email address"
+                       value={this.state.email}
+                       onChange={this.handleChange}/><br/>
                 <input type="password" name="password"
                        className="form-control"
-                       placeholder="Password"/><br/>
+                       placeholder="Password"
+                       value={this.state.password}
+                       onChange={this.handleChange}/><br/>
               </div>
               <button onClick={this.authenticate} type="button"
                       className="btn btn-primary">
@@ -39,4 +84,4 @@ class Admin extends Component {
   }
 }
 
-export default Admin;
+export default AdminPortal;
