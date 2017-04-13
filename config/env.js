29cdc73c1f422751e869caed5c18b9803a9e28fa
Robby Mitchell <robjmitch@gmail.com>
2017-04-12 22:00:33 -0400

    Adds the api route to the env config

--- config/env.js
+++ config/env.js
@@ -8,7 +8,7 @@ function getApiUrl() {
   if (process.env.NODE_ENV === 'production') {
     return "'http://robjmitch.com/api'";
   } else {
-    return "'http://localhost:3001'";
+    return "'http://localhost:3001/api'";
   }
 }
 
